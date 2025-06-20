c8b7194bfd02f1c9953804322d774d931da96d70
Soumik Saha <soumiksaha827@gmail.com>
2025-06-20 21:25:42 +0530

    Implement Dialog component

--- src/modules/dashboard/ui/components/dashboard-navbar.tsx
+++ src/modules/dashboard/ui/components/dashboard-navbar.tsx
@@ -35,7 +35,7 @@ export const DashboardNavbar = () => {
                     <SearchIcon />
                     Search
                     <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-90">
-                        <span className="text-xs">&#8984;</span>
+                        <span className="text-xs">&#8984; + k</span>
                     </kbd>
                 </Button>
             </nav>
