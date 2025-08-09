dba5c44a0bea4225fce86bc1c5f961f47e6f1a18
Soumik Saha <soumiksaha827@gmail.com>
2025-08-09 14:17:12 +0530

    Setup meeting variants

---
+++ src/modules/meetings/ui/components/active-state.tsx
@@ -0,0 +1,32 @@
+import { EmptyState } from "@/components/empty-state"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+import { VideoIcon } from "lucide-react"
+
+interface Props {
+    meetingId: string;
+}
+
+export const ActiveState = ({
+    meetingId,
+}: Props) => {
+    return (
+        <div className="bg-white rounded-lg px-4 py-5 flex-col gap-y-8 items-center justify-center">
+            <EmptyState
+                image="/upcoming.svg"
+                title="Meeting is active"
+                description="Meeting will end once all participants leave."
+            />
+            <div className="flex flex-col-reverse lg:flex-row lg:justify-center items-center gap-4 w-full py-8">
+                <Button asChild
+                    className="w-full lg:w-auto"
+                >
+                    <Link href={`/call/${meetingId}`}>
+                        <VideoIcon />
+                        Join Meeting
+                    </Link>
+                </Button>
+            </div>
+        </div>
+    )
+}
